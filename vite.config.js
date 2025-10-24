1e2be31c3dc94decc0cfa71115606c229b624199
dmnderanda263-png <dmnderanda263@gmail.com>
2025-10-24 10:56:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // ✅ Crucial for relative paths on Vercel
+  base: './', // ✅ ensures assets load properly on Vercel
 })
